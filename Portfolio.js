c7aab24f96404ca3ce33092a03d456276c0ba95c
mani-zstk261 <thavamani.v@zohocorp.com>
2023-03-21 18:26:29 +0530

    2nd

--- Portfolio.js
+++ Portfolio.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["UI Developer", " Web Developer"],
+    strings: ["UI Developer", " Web Developer", "Software Tester"],
     typeSpeed: 90,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["UI Developer ", " Web Developer"],
+    strings: ["UI Developer ", " Web Developer", "Software Tester"],
     typeSpeed: 90,
     backSpeed: 60,
     loop: true,
